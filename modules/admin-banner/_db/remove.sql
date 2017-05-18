f3c462731ca3032fbe5ef53ad863e46185921ab4
iqbalfn <iqbalfawz@gmail.com>
2017-05-18 13:31:53 +0700

    Replace remover query

--- modules/admin-banner/_db/remove.sql
+++ modules/admin-banner/_db/remove.sql
@@ -1,15 +1,5 @@
 DELETE FROM `user_perms_chain` WHERE `user_perms` IN (
-    SELECT `id` FROM `user_perms` WHERE `name` IN (
-        'read_banner',
-        'update_banner',
-        'delete_banner',
-        'create_banner'
-    )
+    SELECT `id` FROM `user_perms` WHERE `group` = 'Banner'
 );
 
-DELETE FROM `user_perms` WHERE `name` IN (
-    'read_banner',
-    'update_banner',
-    'delete_banner',
-    'create_banner'
-);
+DELETE FROM `user_perms` WHERE `group` = 'Banner';
